0d2f4a8a3855e43fccec135fb0587c1f199a9a9e
rvverdug <rvverdug@gmail.com>
2023-07-01 17:04:56 +0200

    attached game over buttons to functions
    Fixed previous error mentioned on gameover script and attached function to buttons

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement; 
 
-public class GameOverScript : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
     public Text pointsText;
 
