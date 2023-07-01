11ae265ed5ca41cc4061ecf4b339810cb695a444
emigu777 <emily.guillou1@gmail.com>
2023-07-02 00:25:37 +0200

    added comments and animations

--- Assets/Scripts/Mushroom_Manager.cs
+++ Assets/Scripts/Mushroom_Manager.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Mushroom_Manager : MonoBehaviour
 {
     [SerializeField] 
     private GameObject _mushroomPrefab;
-    
-    [SerializeField] 
-    private int _instances = 10;
+
+    private int _instances;
     
 
-    // A mushroom is randomly instantiated at the beginning
+    // A mushroom is randomly instantiated at the beginning of the level;
+    // for the first level there are 10 and for the second 5 mushrooms on the terrain at all times
     void Start()
     {
         Instantiate(_mushroomPrefab,  new Vector3(Random.Range(-380f, 380f), 0f, Random.Range(40f, 800f)), Quaternion.identity);
-
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Level1")
+        {
+            _instances = 10;
+        }
+        if (scene.name == "Level2")
+        {
+            _instances = 5;
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        //When there are less mushrooms than wanted, more mushrooms are created randomly in the terrain
+        //When there are less mushrooms than wanted, more mushrooms are created randomly on the terrain
         while (_instances > GameObject.FindGameObjectsWithTag("Mushroom").Length)
         {
             Instantiate(_mushroomPrefab,  new Vector3(Random.Range(-380f, 380f), 0f, Random.Range(40f, 800f)), Quaternion.identity);
--- Assets/Scripts/Spider_Manager.cs
+++ Assets/Scripts/Spider_Manager.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spider_Manager : MonoBehaviour
 {
     [SerializeField] 
     private GameObject _spiderPrefab;
 
-    [SerializeField] 
-    private int _instances = 5;
+    private int _instances;
     
 
-    // A mushroom is randomly instantiated at the beginning
+    // One spider is randomly instantiated at the beginning
+    // for the first level there are 6 and for the second 10 spiders on the terrain at all times
     void Start()
     {
         Instantiate(_spiderPrefab,  new Vector3(Random.Range(-380f, 380f), 0f, Random.Range(40f, 800f)), Quaternion.identity);
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "Level1")
+        {
+            _instances = 6;
+        }
+        if (scene.name == "Level2")
+        {
+            _instances = 10;
+        }
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        //When there are less mushrooms than wanted, more mushrooms are created randomly in the terrain
+        //When there are less spiders than wanted, more spiders are created randomly on the terrain
         while (_instances > GameObject.FindGameObjectsWithTag("Spider").Length)
         {
             Instantiate(_spiderPrefab,  new Vector3(Random.Range(-380f, 380f), 0f, Random.Range(40f, 800f)), Quaternion.identity);
